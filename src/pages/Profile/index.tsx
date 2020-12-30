0268d961bb5772739ea6c9ca8f05e7d17c36c31e
vitorrrocha <vitorrochaflorip@hotmail.com>
2020-12-29 21:05:18 -0300

    fix: ProfileData create

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from './styles';
+
+import ProfileData from '../../components/ProfileData';
 
 const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData 
+            username={'Vitorrrocha'}
+            name={'Vitor Rocha'}
+            avatarUrl={'https://avatars1.githubusercontent.com/u/44412471?s=460&u=476f0458df2f971a353b1c2c288192ca89bb6f86&v=4'}
+            followers={1320}
+            following={320}
+            company={'Searching a job :)'}
+            location={'Florianópolis, SC ,Brasil'}
+            email={'teste@hotmail.com'}
+            blog={undefined}
+          />
+
+        </LeftSide>
+
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
 }
 
 export default Profile;
